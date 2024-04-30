73a4ddc667ce7e9c0266b12b73a6d7fd084db1c2
Elena Ysabel <160290801+elenayasay@users.noreply.github.com>
2024-05-01 04:26:07 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<div className='bg-red-400 inline-block w-32 h-32'></div>
+<div className='bg-blue-400 inline-block w-32 h-32'></div>
+<div className='bg-green-400 inline-block w-32 h-32'></div>
+    </>
+  )
+}
+
+export default App
